6fa104aa4b90587d556f2c0a9adac2349e56de21
Lucas Barbosa de Sousa <lucas.barbosa.sousa@ccc.ufcg.edu.br>
2021-09-03 14:31:57 -0300

    feat: add basic reduxOnly reference

--- reduxOnly/reduxtest.js
+++ reduxOnly/reduxtest.js
@@ -1,1 +1,34 @@
-const eu = 'Lucas';
+const { createStore } = require('redux');
+
+const initialState = {
+  age: 21,
+};
+
+const myReducer = (state = initialState, action) => {
+  const newState = { ...state };
+
+  if (action.type === 'ADD') {
+    newState.age += action.val;
+  }
+  if (action.type === 'SUBTRACT') {
+    newState.age -= action.val;
+  }
+
+  return newState;
+};
+
+const store = createStore(myReducer);
+
+// Whenever the state changes, this happens
+store.subscribe(() => {
+  console.log('State has changes');
+});
+
+console.log(`Initial state: ${store.getState().age}`);
+store.dispatch({ type: 'ADD', val: 10 });
+console.log(`After add: ${store.getState().age}`);
+
+store.dispatch({ type: 'SUBTRACT', val: 10 });
+console.log(`After subtract: ${store.getState().age}`);
+
+console.log(JSON.stringify(store.getState()));
